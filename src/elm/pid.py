773e09cafb54104f0977c1fafd3cd6c3734258b4
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-05-26 14:29:35 -0400

    as_dict option for PIDTable

--- src/elm/pid.py
+++ src/elm/pid.py
@@ -3,10 +3,19 @@ import csv
 PID_TABLE_PATH = "pid_table.csv"
 
 
+class PIDTable(list):
+    def as_dict(self):
+        if not hasattr(self, "_dict"):
+            self._dict = {row["pid"]: row for row in self}
+        return self._dict
+
+
 def load_pid_table():
     with open(PID_TABLE_PATH, "r", newline="") as f:
         reader = csv.reader(f)
         headers = next(reader)
         data = list(reader)
 
-    return [dict(zip(headers, row)) for row in data]
+    headers = [h.lower().replace(" ", "_") for h in headers]
+
+    return PIDTable(map(lambda row: dict(zip(headers, row)), data))
--- tests/test_pid.py
+++ tests/test_pid.py
@@ -6,10 +6,31 @@ def test_load_pid_table():
     assert len(table) >= 1
 
     keys = list(table[0].keys())
-    assert "PID" in keys
-    assert "Data bytes" in keys
-    assert "Description" in keys
-    assert "Min" in keys
-    assert "Max" in keys
-    assert "Units" in keys
-    assert "Formula" in keys
+    assert "pid" in keys
+    assert "data_bytes" in keys
+    assert "description" in keys
+    assert "min" in keys
+    assert "max" in keys
+    assert "units" in keys
+    assert "formula" in keys
+
+
+def test_load_pid_table_as_dict():
+    table = load_pid_table()
+    assert len(table) >= 1
+
+    data = table.as_dict()
+
+    for pid in map(lambda row: row["pid"], table):
+        assert pid in data.keys()
+
+    row = data[table[0]["pid"]]
+    keys = list(row.keys())
+
+    assert "pid" in keys
+    assert "data_bytes" in keys
+    assert "description" in keys
+    assert "min" in keys
+    assert "max" in keys
+    assert "units" in keys
+    assert "formula" in keys
