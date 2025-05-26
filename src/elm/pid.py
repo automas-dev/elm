334f519c2bb2b233b81a3bf71b60d939f2ffbc6b
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-05-26 14:33:11 -0400

    Cache pid table after load

--- src/elm/pid.py
+++ src/elm/pid.py
@@ -3,6 +3,9 @@ import csv
 PID_TABLE_PATH = "pid_table.csv"
 
 
+_pid_table = None
+
+
 class PIDTable(list):
     def as_dict(self):
         if not hasattr(self, "_dict"):
@@ -11,11 +14,16 @@ class PIDTable(list):
 
 
 def load_pid_table():
-    with open(PID_TABLE_PATH, "r", newline="") as f:
-        reader = csv.reader(f)
-        headers = next(reader)
-        data = list(reader)
+    global _pid_table
 
-    headers = [h.lower().replace(" ", "_") for h in headers]
+    if _pid_table is None:
+        with open(PID_TABLE_PATH, "r", newline="") as f:
+            reader = csv.reader(f)
+            headers = next(reader)
+            data = list(reader)
 
-    return PIDTable(map(lambda row: dict(zip(headers, row)), data))
+        headers = [h.lower().replace(" ", "_") for h in headers]
+
+        _pid_table = PIDTable(map(lambda row: dict(zip(headers, row)), data))
+
+    return _pid_table
