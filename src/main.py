309c6b639d97de9e6276a50e68c465903358f461
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-05-29 22:53:53 -0400

    Dump service 01 responses

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import json
+
 from elm import ElmClient
 
 if __name__ == "__main__":
@@ -12,3 +14,14 @@ if __name__ == "__main__":
 
     pid_support = c.scan_pid_support()
     print("Supported PID are", pid_support)
+
+    # Read supported by all
+    all_support_data = {}
+    for pid in pid_support:
+        data = c.obd_command(1, pid, return_raw=True)
+        all_support_data[pid] = data
+
+    print(all_support_data)
+
+    with open("kia_pid_01_dump.json", "w") as f:
+        json.dump(all_support_data, f, indent=2)
